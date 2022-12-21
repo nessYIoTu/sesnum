7b90dec642be57317ff24d6c1416ebdaf4987ba8
nessYIoTu <101914470+nessYIoTu@users.noreply.github.com>
2022-12-21 17:07:03 +0100

    Update myFirstfile.c

--- myFirstfile.c
+++ myFirstfile.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-
-int is_palindrome(char *str) {
-    int len = strlen(str);
-    for (int i = 0; i < len / 2; i++) {
-        if (str[i] != str[len - i - 1]) {
-            return 0; // false
-        }
-    }
-    return 1; // true
+int main()
+{
+int a = 10;
+a = a + 1;
+printf("La valeur de a est %d \n", a);
+return 0;
 }
-
-int main() {
-    char str1[] = "racecar";
-    char str2[] = "hello";
-
-    printf("%s is a palindrome: %d\n", str1, is_palindrome(
